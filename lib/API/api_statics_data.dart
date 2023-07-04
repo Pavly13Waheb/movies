d12368688deac3951659ad250e9ab1bae9d87163
pavly_waheb_13 <pavlywaheeb@gmail.com>
2023-07-04 22:04:14 +0300

    still working

--- lib/API/api_statics_data.dart
+++ lib/API/api_statics_data.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
-import 'api_popular_class_data.dart';
+import 'package:movies/API/api_latest_data/api_latest_movie_data_model.dart';
 import 'package:http/http.dart';
+import 'package:movies/API/popular_api/api_popular_class_data.dart';
 
 class ApiStaticsManager {
   static String apiKeyMovie = "c2d29417c7c637ee6313364c9954b0cc";
   static String apiMovieBaseUrl = "api.themoviedb.org";
   static String apiMoviePopularUrl = "3/movie/popular";
   static String apiMovieTmdbImageUrl = "http://image.tmdb.org/t/p/w500";
+  static String apiMovieLatestUrl = "3/movie/latest";
 
   static Future<ApiPopularDM> getPopularData() async {
     Uri url =
@@ -16,5 +18,15 @@ class ApiStaticsManager {
     ApiPopularDM apiPopularDM = ApiPopularDM.fromJson(json);
 
     return apiPopularDM;
+  }
+
+  static Future<ApiLatestMovieDM> getLatestData() async {
+    Uri url =
+    Uri.http(apiMovieBaseUrl, apiMovieLatestUrl, {"api_key": apiKeyMovie});
+    Response response = await get(url);
+    Map json = jsonDecode(response.body);
+    ApiLatestMovieDM apiLatestDM = ApiLatestMovieDM.fromJson(json);
+
+    return apiLatestDM;
   }
 }
