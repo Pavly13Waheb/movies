83582ba94632462356c2174b3f8ec616330a53f9
pavly_waheb_13 <pavlywaheeb@gmail.com>
2023-07-08 21:53:41 +0300

    still working

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,13 @@ import 'package:movies/theme/app_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'home_screen/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'home_screen/navbar_home_screens/home_tab/popular_movie_details/popular_movie_details.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppProvider(),
